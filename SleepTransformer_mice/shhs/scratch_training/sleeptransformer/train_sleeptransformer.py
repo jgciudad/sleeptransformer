ba7742e089d15569b2cc3ebaf2799fe258d48cdc
jgciudad <jgciudad@outlook.com>
2023-06-05 19:34:30 +0200

    test 1 training epoch

--- SleepTransformer_mice/shhs/scratch_training/sleeptransformer/train_sleeptransformer.py
+++ SleepTransformer_mice/shhs/scratch_training/sleeptransformer/train_sleeptransformer.py
@@ -84,7 +84,8 @@ config.frame_seq_len = FLAGS.frame_seq_len
 config.frm_maxlen = FLAGS.frame_seq_len
 config.epoch_seq_len = FLAGS.seq_len
 config.seq_maxlen = FLAGS.seq_len
-config.training_epoch = FLAGS.training_epoch*config.epoch_seq_len
+# config.training_epoch = FLAGS.training_epoch*config.epoch_seq_len
+config.training_epoch = 1
 
 if (FLAGS.num_blocks > 0):
     config.frm_num_blocks = FLAGS.num_blocks
