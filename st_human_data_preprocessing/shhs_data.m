e5423a44c6f148416cb28b874267156f5728e2f9
jgciudad <jgciudad@outlook.com>
2023-07-31 01:03:04 +0200

    prueba matlab

--- st_human_data_preprocessing/shhs_data.m
+++ st_human_data_preprocessing/shhs_data.m
@@ -22,8 +22,8 @@ N = numel(dirlist);
 % fprintf(fid, num2str(N));
 % fclose(fid);
 
-% parfor n = 1 : N
-for n = 1 : N
+parfor n = 1 : N
+% for n = 1 : N
     filename = dirlist(n).name;
     disp(filename);
     process_and_save_1file(filename, n, xml_path, edf_path, mat_path, fs, n_classes);
