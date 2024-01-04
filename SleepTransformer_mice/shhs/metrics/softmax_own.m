de91cda1ddb254fab476dec043611272f0b523e4
jgciudad <jgciudad@outlook.com>
2024-01-04 17:03:23 +0100

    changed name so it's not mixed up with matlab native softmax function

---
+++ SleepTransformer_mice/shhs/metrics/softmax_own.m
@@ -0,0 +1,8 @@
+function prob = softmax_own(score)
+    score_ = score;    
+    score_ = exp(score_);
+    prob = zeros(size(score_));
+    for i = 1 : size(score_,1)
+        prob(i,:) = score_(i,:)/sum(score_(i,:));
+    end
+end
